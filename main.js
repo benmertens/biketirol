97f539f1458cd4a0ff50a5600b6943c71f285784
benmertens <ben.mertens@student.uibk.ac.at>
2025-05-22 16:58:18 +0200

    window location hinzugefügt

--- main.js
+++ main.js
@@ -59,8 +59,19 @@ for (let etappe of ETAPPEN) {
     //console.log(etappe.user);
     //console.log(etappe.nr);
     //console.log(etappe.titel);
+    let selected = "";
+    if (etappe.nr == 25) {
+        selected = "selected";
+    }
     pulldown.innerHTML += `
-        <option value="${etappe.user}">Etappe ${etappe.nr}: ${etappe.titel}</option>
+        <option ${selected} value ="${etappe.user}">Etappe ${etappe.nr}: ${etappe.titel}</option>
     `;
 
+}
+
+
+// auf Wechsel in Pulldown reagieren
+pulldown.onchange = function(evt) {
+    console.log(evt.target.value);
+    window.location.href = `https://${evt.target.value}.github.io/biketirol`;
 }
